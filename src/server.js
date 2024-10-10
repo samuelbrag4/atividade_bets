a5d32e86f5df357c4f5443354ce00f06c49d4104
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-10-10 15:02:50 -0300

    feat: route development

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { config } from "dotenv";
+
+import routes from "./routes/index.routes.js";
+
+config();
+
+const serverPort = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(serverPort, () => {
+  console.log(`🚀 Server started on http://localhost:${serverPort}`);
+});
